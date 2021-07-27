b90354c379a4f17945967f286ca65a3a18644e9d
paponkoushik <paponkoushik@gmail.com>
2021-07-27 12:48:21 +0600

    vue router install, boostrap installed and login page setup

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="mt-5">
+      <div class="card">
+        <div class="card-header text-center">Login</div>
+        <div class="card-body">
+          <form>
+            <div class="form-group row align-items-center">
+              <label class="col-sm-2 mb-0">Email</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="user.email" required>
+              </div>
+            </div>
+            <div class="form-group row align-items-center">
+              <label class="col-sm-2 mb-0">Password</label>
+              <div class="col-sm-10">
+                <input type="password" class="form-control" v-model="user.password" required>
+              </div>
+            </div>
+          </form>
+          <button class="btn btn-outline-primary float-md-right"
+                  type="submit"
+                  @click.prevent="">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      user: {},
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import Login from "../components/Auth/Login";
+
+export const routes = [
+    { path: '/', component: Login },
+]
